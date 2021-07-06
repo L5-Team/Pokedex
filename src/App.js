4f2698552d8cd90ba32bfcd9dbbe64049a90ac13
Lucas <dexterfail@hotmail.com>
2021-07-06 10:08:11 -0300

    Create header and search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import "./App.css";
+import Routes from './Routes'
 
 export default function App() {
  return (
-   <div>
-     <h1>Hello world KKKKKK</h1>
-   </div>
+   <>
+     <Routes/>
+   </>
  );
 }
