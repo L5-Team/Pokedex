bfd6deac80dc80cd9db12a9acba2d739658a46ba
Lucas <dexterfail@hotmail.com>
2021-07-08 16:20:55 -0300

    show all pokemons,with nextUrl and types

--- src/App.js
+++ src/App.js
@@ -1,11 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { getAllPokemon, getPokemon } from "./services/pokemon";
 import "./App.css";
-import Routes from './Routes'
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
+// import Routes from './Routes'
 
 export default function App() {
- return (
-   <>
-     <Routes/>
-   </>
- );
+    const [pokemonData, setPokemonData] = useState([]);
+    const [nextUrl, setNextUrl] = useState("");
+    const [prevUrl, setPrevUrl] = useState("");
+    const [loading, setLoading] = useState(true);
+    const initialUrl = "https://pokeapi.co/api/v2/pokemon";
+
+    useEffect(() => {
+        async function fetchData() {
+            let response = await getAllPokemon(initialUrl);
+            setNextUrl(response.next);
+            setPrevUrl(response.next);
+            let pokemon = await loadingPokemon(response.results);
+            setLoading(false);
+        }
+        fetchData();
+    }, []);
+
+    const next = async () => {
+        setLoading(true);
+        let data = await getAllPokemon(nextUrl);
+        await loadingPokemon(data.results);
+        setNextUrl(data.next);
+        setPrevUrl(data.previous);
+        setLoading(false);
+    };
+
+    const prev = async () => {
+        if (!prevUrl) return;
+        setLoading(true);
+        let data = await getAllPokemon(prevUrl);
+        await loadingPokemon(data.results);
+        setNextUrl(data.next);
+        setPrevUrl(data.previous);
+        setLoading(false);
+    };
+
+    const loadingPokemon = async (data) => {
+        let _pokemonData = await Promise.all(
+            data.map(async (pokemon) => {
+                let pokemonRecord = await getPokemon(pokemon.url);
+                return pokemonRecord;
+            })
+        );
+        setPokemonData(_pokemonData);
+    };
+    console.log(pokemonData);
+    return (
+        <>
+            <Navbar />
+            {loading ? (
+                <h1>Loading</h1>
+            ) : (
+                <>
+                    <div className="btn">
+                        <button onClick={prev}>Prev</button>
+                        <button onClick={next}>Next</button>
+                    </div>
+                    <div className="grid-container">
+                        {pokemonData.map((pokemon, i) => {
+                            return <Card key={i} pokemon={pokemon} />;
+                        })}
+                    </div>
+                    <div className="btn">
+                        <button onClick={prev}>Prev</button>
+                        <button onClick={next}>Next</button>
+                    </div>
+                </>
+            )}
+        </>
+    );
 }
